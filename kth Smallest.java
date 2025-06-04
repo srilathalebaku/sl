ca959beb026621e3a75c84d06ace59b85360d6ba
srilathalebaku <srilatha8749@gmail.com>
2025-06-04 10:54:44 +0530

    Create kth Smallest.java

---
+++ kth Smallest.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = countLessEqual(matrix, mid, n);
+
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    // Helper to count elements ≤ target
+    private int countLessEqual(int[][] matrix, int target, int n) {
+        int count = 0;
+        int row = n - 1, col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= target) {
+                count += row + 1;
+                col++;
+            } else {
+                row--;
+            }
+        }
+
+        return count;
+    }
+}
